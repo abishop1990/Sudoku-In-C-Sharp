40271658c73f4c298a77cf3a79b5e5bef5f008b9
Alan Bishop <djab1990@gmail.com>
2014-06-14 14:09:27 -0700

    Command Line Menus, Generation Code Shells.

--- Sudoku/Sudoku.cs
+++ Sudoku/Sudoku.cs
@@ -2,20 +2,125 @@
 
 class Sudoku
 {
-    static void Main(string[] args)
-    {
-        WelcomeScreen();
-        Console.ReadKey();
-        Board gameBoard = new Board();
-        Console.Write(gameBoard);
-    }
+	static void Main(string[] args)
+	{
+		WelcomeScreen();
+		MainMenu();
+		//Board gameBoard = new Board();
+		//Console.Write(gameBoard);
+	}
 
-    static void WelcomeScreen()
-    {
-        Console.WriteLine("Welcome to Sudoku!");
-        Console.WriteLine("Written By Alan Bishop");
-        Console.WriteLine("\nPress Any Key To Continue");
-    }
+	static void WelcomeScreen()
+	{
+		Console.Clear();
+		Console.WriteLine("Welcome to Simple Sudoku!");
+		Console.WriteLine("Version 0.1");
+		Console.WriteLine("Written By Alan Bishop");
+		PromptToKeyPress();
+	}
+
+	static void PromptToKeyPress()
+	{
+		Console.WriteLine("\nPress Any Key To Continue");
+		Console.ReadKey();
+		Console.Clear();
+	}
+
+	static void MainMenu()
+	{
+		while (true)
+		{
+			Console.WriteLine("Select An Option:");
+			Console.WriteLine("1. New Puzzle");
+			Console.WriteLine("2. How To Play");
+			Console.WriteLine("3. Exit");
+			Console.Write("Enter Selection: ");
+			string cmd = Console.ReadLine();
+			int cmdN = 0;
+			if (!Int32.TryParse(cmd, out cmdN) || cmdN > 3 || cmdN < 1)
+			{
+				DisplayInputError();
+				continue;
+			}
+			if (cmdN == 1) { NewPuzzle(); }
+			else if (cmdN == 2) { DisplayHelp(); }
+			else Environment.Exit(0);
+		}
+	}
+
+	static void DisplayHelp()
+	{
+		Console.Clear();
+		Console.WriteLine("Solve a 9x9 grid of numbers by making every, row, column and 3x3 grid contain numbers 1-9");
+		Console.WriteLine("Input done via command prompt (Ex: 'A B 4' would place a 4 in row A, column B)");
+		Console.WriteLine("Type 'X' to have the computer tell you if you have any errors");
+		Console.WriteLine("Type 'S' to have computer solve the puzzle for you");
+		Console.WriteLine("Type '?' for help");
+		Console.WriteLine("Type 'Q' to quit the program during gameplay");
+		PromptToKeyPress();
+	}
+
+
+	static void NewPuzzle()
+	{
+		Board solvedBoard = newSolvedBoard();
+		Board puzzleBoard = buildPuzzleBoard(solvedBoard,1);
+		bool solved = false;
+		while(!solved)
+		{
+			Console.Clear();
+			Console.WriteLine(puzzleBoard.ToString() + "\n");
+			Console.Write("Enter command (Type '?' for help): ");
+			string cmd = Console.ReadLine();
+			cmd = cmd.ToUpper();
+			if (cmd[0] == 'Q') Environment.Exit(0);
+			else if (cmd[0] == '?') DisplayHelp();
+			else
+			{
+				DisplayInputError();
+				continue;
+			}
+		}
+	}
+
+	static void DisplayInputError()
+	{
+		Console.WriteLine("Invalid Command, Try Again!");
+		PromptToKeyPress();
+	}
+
+	public static Board buildPuzzleBoard(Board solvedBoard, int level)
+	{
+		Board newBoard = solvedBoard.copyBoard();
+
+		switch (level)
+		{
+			case 1:
+
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			default:
+				//Just do easy
+				break;
+		}
+
+		return newBoard;
+	}
+
+	public static Board newSolvedBoard()
+	{
+		Board newBoard = new Board();
+
+		return newBoard;
+	}
 
 }
 
--- Sudoku/SudokuBoard.cs
+++ Sudoku/SudokuBoard.cs
@@ -10,141 +10,161 @@ using System;
 
 class Board
 {
-    private int[,] cells;
-    private bool solved;
+	private int[,] cells;
+	private bool solved;
 
-    public Board()
-    {
-        InitGrid();
-        solved = false;
-    }
+	public Board() { InitGrid(); solved = false; }
 
-    public bool isSolved() { return solved; }
+	private void InitGrid()
+	{
+		cells = new int[9, 9];
+		for (int i = 0; i < 9; ++i)
+		{
+			for (int j = 0; j < 9; ++j) { cells[i, j] = 0; }
+		}
+	}
 
-    /* Checks rows, columns, diagonals, grids, updates solved bool */
-    //(If there are 0s AKA blanks, will return false)
-    public bool CheckSolved()
-    {
-        if (!CheckRows() || !CheckColumns() || !CheckGrids())
-        {
-            solved = false;
-        }
-        else solved = true;
-        return solved;
-    }
+	public int GetCell(int row, int column) { return cells[row, column]; }
+	public void SetCell(int row, int column, int val)
+	{
+		if (val > 9 || val < 0) return;
+		cells[row, column] = val;
+	}
+	public bool SetCellAndCheckSolved(int row, int column, int val)
+	{
+		SetCell(row, column, val);
+		return CheckSolved();
+	}
+	public bool IsSolved() { return solved; }
 
-    //Check each row for if it has a valid solution
-    //(If there are 0s AKA blanks, will return false)
-    private bool CheckRows()
-    {
-        bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
-        for (int i = 0; i < 9; ++i)
-        {
-            //Init check values
-            for (int k = 1; k < 10; ++k) { hasValue[k] = false; }
-            for (int j = 0; j < 9; ++j)
-            {
-                if (hasValue[cells[i, j]] == true) { return false; }
-                else hasValue[cells[i, j]] = true;
-            }
-        }
-        return true;
-    }
-    //Check each column for if it has a valid solution
-    //Pretty much copy-pasta of checkRows, only indexing col/row switched
-    //(If there are 0s AKA blanks, will return false)
-    private bool CheckColumns()
-    {
-        bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
-        for (int i = 0; i < 9; ++i)
-        {
-            //Init check values
-            for (int k = 1; k < 10; ++k) { hasValue[k] = false; }
-            for (int j = 0; j < 9; ++j)
-            {
-                if (hasValue[cells[j, i]] == true) { return false; }
-                else hasValue[cells[j, i]] = true;
-            }
-        }
-        return true;
-    }
+	///////////////////////////SOLVING RELATED STUFF//////////////////////////////////////////////////
 
-    //Check each 3x3 box for if it has a valid solution
-    //(If there are 0s AKA blanks, will return false)
-    private bool CheckGrids()
-    {
-        bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
+	/* Checks rows, columns, diagonals, grids, updates solved bool */
+	//(If there are 0s AKA blanks, will return false)
+	public bool CheckSolved()
+	{
+		if (!CheckRows() || !CheckColumns() || !CheckGrids()) { solved = false; }
+		else solved = true;
+		return solved;
+	}
 
-        //Each grid is a 3x3 that must contain each 1-9 element
-        int xstart = 0;
-        int ystart = 0;
-        for (int i = 0; i < 3; ++i)
-        {
-            for (int j = 0; j < 3; ++j)
-            {
-                if (CheckSubGrid(xstart, ystart) == false) return false;
-                ystart += 3;
-            }
-            ystart = 0;
-            xstart += 3;
-        }
-        return true;
-    }
+	//Check each row for if it has a valid solution
+	//(If there are 0s AKA blanks, will return false)
+	private bool CheckRows()
+	{
+		bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
+		for (int i = 0; i < 9; ++i)
+		{
+			//Init check values
+			for (int k = 1; k < 10; ++k) { hasValue[k] = false; }
+			for (int j = 0; j < 9; ++j)
+			{
+				if (hasValue[cells[i, j]] == true) { return false; }
+				else hasValue[cells[i, j]] = true;
+			}
+		}
+		return true;
+	}
+	//Check each column for if it has a valid solution
+	//Pretty much copy-pasta of checkRows, only indexing col/row switched
+	//(If there are 0s AKA blanks, will return false)
+	private bool CheckColumns()
+	{
+		bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
+		for (int i = 0; i < 9; ++i)
+		{
+			//Init check values
+			for (int k = 1; k < 10; ++k) { hasValue[k] = false; }
+			for (int j = 0; j < 9; ++j)
+			{
+				if (hasValue[cells[j, i]] == true) { return false; }
+				else hasValue[cells[j, i]] = true;
+			}
+		}
+		return true;
+	}
 
-    private bool CheckSubGrid(int xstart, int ystart)
-    {
-        bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
-        for (int i = xstart; i < xstart + 3; ++i)
-        {
-            for (int j = ystart; i < ystart + 3; ++j)
-            {
+	//Check each 3x3 box for if it has a valid solution
+	//(If there are 0s AKA blanks, will return false)
+	private bool CheckGrids()
+	{
+		bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
 
-            }
-        }
-        return true;
-    }
+		//Each grid is a 3x3 that must contain each 1-9 element
+		int xstart = 0;
+		int ystart = 0;
+		for (int i = 0; i < 3; ++i)
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				if (CheckSubGrid(xstart, ystart) == false) return false;
+				ystart += 3;
+			}
+			ystart = 0;
+			xstart += 3;
+		}
+		return true;
+	}
+
+	private bool CheckSubGrid(int xstart, int ystart)
+	{
+		bool[] hasValue = new bool[10]; //Checking for this value (Ex: hasValue[1] = there is a 1 in row)
+		for (int i = xstart; i < xstart + 3; ++i)
+		{
+			for (int j = ystart; i < ystart + 3; ++j)
+			{
+				if (hasValue[cells[j, i]] == true) { return false; }
+				else hasValue[cells[j, i]] = true;
+			}
+		}
+		return true;
+	}
+
+	/////////////////////////////END SOLVING RELATED STUFF///////////////////////////////////////////////////
 
 
-    /* Okay I admit this is pretty ugly but I'm going to go GUI so this is just temporary..*/
-    public override string ToString()
-    {
-        string str = "";
-        string rowSeperator = " |-----------|-----------|-----------| \n";
-        string columnSeperator = " | ";
-        str += "   ";
-        for (int i = 0; i < 9; ++i)
-        {
-            char column = (char)(((int)'A') + i);
-            str += " " + column + " ";
-            if ((i + 1) % 3 == 0) str += "   ";
-        }
-        str += "\n";
-        char row = 'A';
-        for (int i = 0; i < 9; ++i)
-        {
-            str += rowSeperator;
-            for (int j = 0; j < 9; ++j)
-            {
-                str += columnSeperator + cells[i, j];
-            }
-            str += columnSeperator;
-            str += row + "\n";
-            row++;
-        }
-        str += rowSeperator;
-        return str;
-    }
+	/* Okay I admit this is pretty ugly but I'm going to go GUI so this is just temporary..*/
+	public override string ToString()
+	{
+		string str = "";
+		string rowSeperator = " |-----------|-----------|-----------| \n";
+		string columnSeperator = " | ";
+		str += "   ";
+		for (int i = 0; i < 9; ++i)
+		{
+			char column = (char)(((int)'A') + i);
+			str += " " + column + " ";
+			if ((i + 1) % 3 == 0) str += "   ";
+		}
+		str += "\n";
+		char row = 'A';
+		for (int i = 0; i < 9; ++i)
+		{
+			str += rowSeperator;
+			for (int j = 0; j < 9; ++j)
+			{
+				str += columnSeperator + cells[i, j];
+			}
+			str += columnSeperator;
+			str += row + "\n";
+			row++;
+		}
+		str += rowSeperator;
+		return str;
+	}
 
-    private void InitGrid()
-    {
-        cells = new int[9, 9];
-        for (int i = 0; i < 9; ++i)
-        {
-            for (int j = 0; j < 9; ++j)
-            {
-                cells[i, j] = 0;
-            }
-        }
-    }
 
+	public Board copyBoard()
+	{
+		Board newBoard = new Board();
+		for (int i = 0; i < 9; ++i )
+		{
+			for(int j = 0; j < 9; ++j)
+			{
+				newBoard.SetCell(i,j,cells[i, j]);
+			}
+		}		
+		newBoard.CheckSolved();
+		return newBoard;
+	}
 }
