a7cb071352a058c90e5f48accf67565fe9cc9415
Alan Bishop <djab1990@gmail.com>
2014-06-25 18:33:36 -0700

    Sudoku now displaying as puzzle instead of just solved board.

--- Sudoku/GUI.cs
+++ Sudoku/GUI.cs
@@ -14,50 +14,39 @@ public partial class GUI : Form
 	public GUI()
 	{
 		InitializeComponent();
-		Board newBoard = SudokuGenerator.NewSolvedBoard();
-		GUI_BuildSudokuGrid(newBoard);
+		Board solvedBoard = SudokuGenerator.NewSolvedBoard();
+		Board newBoard = SudokuGenerator.buildPuzzleBoard(solvedBoard, 1);
+		GUI_BuildSudokuGrid();
+		GUI_LoadBoard(newBoard);
 	}
 
 	private DataGridView sudokuGrid;
-	private DataGridViewTextBoxColumn [] sudokuColumns;
-	public void GUI_BuildSudokuGrid(Board board)
+	private DataGridViewTextBoxColumn[] sudokuColumns;
+	public void GUI_BuildSudokuGrid()
 	{
 		this.sudokuGrid = new System.Windows.Forms.DataGridView();
 		this.sudokuColumns = new DataGridViewTextBoxColumn[9];
-		for (int column = 0; column < 9; ++column )
+		for (int column = 0; column < 9; ++column)
 		{
 			sudokuColumns[column] = new System.Windows.Forms.DataGridViewTextBoxColumn();
-			sudokuColumns[column].HeaderText =  "" + (char)((int)'A' + column);
+			sudokuColumns[column].HeaderText = "" + (char)((int)'A' + column);
 			sudokuColumns[column].MaxInputLength = 1;
 			sudokuColumns[column].Name = sudokuColumns[column].HeaderText;
 			sudokuColumns[column].Width = 50;
 			sudokuColumns[column].DefaultCellStyle.Font = new Font("Arial", 32F, GraphicsUnit.Pixel);
 		}
-		
+
 		((System.ComponentModel.ISupportInitialize)(this.sudokuGrid)).BeginInit();
 
-		
+
 		this.sudokuGrid.ColumnHeadersVisible = false;
 		this.sudokuGrid.Columns.AddRange(sudokuColumns);
 		//Put cells into board
 		for (int column = 0; column < 9; ++column)
 		{
 			string[] colCells = new string[9];
-			for (int row = 0; row < 9; ++row) 
-			{ 
-				colCells[row] = board.GetCellString(row, column); 
-			}
+			for (int row = 0; row < 9; ++row) { colCells[row] = "0"; }
 			sudokuGrid.Rows.Add(colCells);
-		}
-		for (int col = 0; col < 9; ++col)
-		{
-			for (int row = 0; row < 9; ++row)
-			{
-				if (sudokuGrid.Rows[row].Cells[col].Value.ToString() != "0")
-				{
-					sudokuGrid.Rows[row].Cells[col].ReadOnly = true;
-				}
-			}
 		}
 
 		sudokuGrid.Location = new System.Drawing.Point(12, 43);
@@ -69,16 +58,33 @@ public partial class GUI : Form
 		sudokuGrid.RowHeadersVisible = false;
 		sudokuGrid.CellBorderStyle = DataGridViewCellBorderStyle.Single;
 		sudokuGrid.Name = "dataGridView1";
-		
+
 		sudokuGrid.RowTemplate.Height = 100;
 		foreach (DataGridViewRow row in sudokuGrid.Rows) { row.Height = 50; }
 		sudokuGrid.Size = new System.Drawing.Size(941, 500);
 		sudokuGrid.TabIndex = 2;
 		sudokuGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-		
-		Controls.Add(this.sudokuGrid); 
+
+		Controls.Add(this.sudokuGrid);
 		((System.ComponentModel.ISupportInitialize)(this.sudokuGrid)).EndInit();
 
+	}
+
+	public void GUI_LoadBoard(Board board)
+	{
+		//Put values into board
+		for (int column = 0; column < 9; ++column)
+		{
+			for (int row = 0; row < 9; ++row)
+			{
+				sudokuGrid.Rows[row].Cells[column].Value = board.GetCellString(row, column);
+				if (sudokuGrid.Rows[row].Cells[column].Value.ToString() != "")
+				{
+					sudokuGrid.Rows[row].Cells[column].ReadOnly = true;
+				}
+				else sudokuGrid.Rows[row].Cells[column].ReadOnly = false;
+			}
+		}
 	}
 
 	private void GUI_Load(object sender, EventArgs e)
